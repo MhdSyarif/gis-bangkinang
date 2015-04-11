edaccff6556324d1d06f674417018ea28ed41ee2
Mhd. Syarif <mhdsyarif.sf@gmail.com>
2015-04-11 22:21:14 +0700

    Update loginsubmit.php

--- loginsubmit.php
+++ loginsubmit.php
@@ -1,9 +1,6 @@
 <?
 session_start();
-$mysql_host = "mysql5.000webhost.com";
-	$mysql_database = "a5703683_m4p";
-	$mysql_user = "a5703683_m4p";
-	$mysql_password = "5y4r!*3f^#";
+include"connect.php";
 
 
 $db = mysql_connect($mysql_host,$mysql_user,$mysql_password) or DIE("Connection is down ");
